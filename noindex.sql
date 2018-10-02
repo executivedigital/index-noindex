07360430814a9d4d0fc79cb976671302296f32d3
petarcvetic <petar.cvetic@gmail.com>
2018-10-03 00:55:05 +0200

    Change .sql

--- noindex.sql
+++ noindex.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 4.6.6deb5
--- https://www.phpmyadmin.net/
+-- version 4.1.14
+-- http://www.phpmyadmin.net
 --
--- Host: localhost:3306
--- Generation Time: Oct 01, 2018 at 06:24 PM
--- Server version: 5.7.23-0ubuntu0.18.04.1
--- PHP Version: 7.2.10-0ubuntu0.18.04.1
+-- Host: 127.0.0.1
+-- Generation Time: Oct 03, 2018 at 12:54 AM
+-- Server version: 5.6.17
+-- PHP Version: 5.5.12
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET time_zone = "+00:00";
@@ -14,7 +14,7 @@ SET time_zone = "+00:00";
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
 /*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8mb4 */;
+/*!40101 SET NAMES utf8 */;
 
 --
 -- Database: `noindex`
@@ -26,11 +26,12 @@ SET time_zone = "+00:00";
 -- Table structure for table `live_sites`
 --
 
-CREATE TABLE `live_sites` (
-  `live_id` int(5) NOT NULL,
+CREATE TABLE IF NOT EXISTS `live_sites` (
+  `live_id` int(5) NOT NULL AUTO_INCREMENT,
   `live_url` varchar(100) NOT NULL,
-  `status` int(1) NOT NULL DEFAULT '0'
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+  `status` int(1) NOT NULL DEFAULT '0',
+  PRIMARY KEY (`live_id`)
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=14 ;
 
 --
 -- Dumping data for table `live_sites`
@@ -46,8 +47,7 @@ INSERT INTO `live_sites` (`live_id`, `live_url`, `status`) VALUES
 (7, 'https://www.badhabitstattoos.com/', 1),
 (8, 'https://boqueriarestaurant.com/', 1),
 (9, 'https://bruceturkel.com/', 1),
-(10, 'https://burgermeistermia.com/', 1),
-(13, 'sfsdfsdf', 1);
+(10, 'https://burgermeistermia.com/', 1);
 
 -- --------------------------------------------------------
 
@@ -55,11 +55,12 @@ INSERT INTO `live_sites` (`live_id`, `live_url`, `status`) VALUES
 -- Table structure for table `stage_sites`
 --
 
-CREATE TABLE `stage_sites` (
-  `stage_id` int(5) NOT NULL,
+CREATE TABLE IF NOT EXISTS `stage_sites` (
+  `stage_id` int(5) NOT NULL AUTO_INCREMENT,
   `stage_url` varchar(100) NOT NULL,
-  `status` int(1) NOT NULL DEFAULT '0'
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+  `status` int(1) NOT NULL DEFAULT '0',
+  PRIMARY KEY (`stage_id`)
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=12 ;
 
 --
 -- Dumping data for table `stage_sites`
@@ -75,39 +76,8 @@ INSERT INTO `stage_sites` (`stage_id`, `stage_url`, `status`) VALUES
 (7, 'http://bhabitstattoo.staging.wpengine.com/', 1),
 (8, 'http://boqueria.staging.wpengine.com/', 1),
 (9, 'http://bruceturkel.staging.wpengine.com/', 1),
-(10, 'https://phoenixtowingservice.com/', 1),
-(11, 'sdfsdf', 1);
+(10, 'https://phoenixtowingservice.com/', 1);
 
---
--- Indexes for dumped tables
---
-
---
--- Indexes for table `live_sites`
---
-ALTER TABLE `live_sites`
-  ADD PRIMARY KEY (`live_id`);
-
---
--- Indexes for table `stage_sites`
---
-ALTER TABLE `stage_sites`
-  ADD PRIMARY KEY (`stage_id`);
-
---
--- AUTO_INCREMENT for dumped tables
---
-
---
--- AUTO_INCREMENT for table `live_sites`
---
-ALTER TABLE `live_sites`
-  MODIFY `live_id` int(5) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=14;
---
--- AUTO_INCREMENT for table `stage_sites`
---
-ALTER TABLE `stage_sites`
-  MODIFY `stage_id` int(5) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=12;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
